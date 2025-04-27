66310c0b155c894d2e41b5d6d716a5011416f2ba
Salmane Issa <143265121+Sgissa@users.noreply.github.com>
2025-04-27 00:18:32 +0000

    added styling on nav + code in function.php to enable style.css and temp index.html

--- wp-content/themes/omniman/functions.php
+++ wp-content/themes/omniman/functions.php
@@ -1,1 +1,15 @@
 <?php
+
+function init_edit_css()
+{
+    add_editor_style("style.css");
+}
+
+add_action("after_setup_theme", "init_edit_css");
+
+function init_view_css()
+{
+    wp_enqueue_style("final-style", get_stylesheet_directory_uri() . "/style.css");
+}
+
+add_action("wp_enqueue_scripts", "init_view_css");
